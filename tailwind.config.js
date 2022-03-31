2763d05e137a59a2cb09d267b110023ff0b8589e
yuuki rey <achmada479@gmail.com>
2022-03-31 19:56:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      "poppins" : ['poppins, sans serif'],
+      "roboto" : ['roboto'],
+    },
+    colors: {
+      "color-base" : "#00cc00",
+      "color-secondary" : "#C27325",
+      "color-first" : "#2F1701",
+      "color-scnd" : "#73491C",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
